9387f7e18362c91c3b31b2e784d335feb28e1ce8
WangHao <poopoo1993@gmail.com>
2018-05-07 09:49:24 +0800

    add function in class.h to print data.

--- class.h
+++ class.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "function.h"
 
 using namespace std;
 
@@ -10,4 +11,14 @@ public:
 	string sName = "";
 	int score[6] = {};
 	double averageScore = 0;
+	
+	void printData(){
+		
+		cout<<sid<<","<<sName<<",";
+		for(int i = 0; i < 6; i++){
+			cout << "	" << score[i];
+		}
+		cout << "	" << averageScore << endl;
+		
+	}
 };
